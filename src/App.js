5fbabe792aa1ac02adee308fadd89cf11d95e747
daffamukti <daffamuktizaelani97@gmail.com>
2022-06-19 22:23:17 +0700

    challange 9

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
-    <div className="App">
-      
+    <div className="App main">
+      <Header title="Your friendly contact app" />
+      <Contact contacts={contacts} />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,25 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className=''>
+            {
+
+                props.contacts.map((contact) => {
+                    return <main>
+                        <aside>
+                            <img src={contact.photo} alt="" />
+                        </aside>
+                        <aside className='center'>
+                            <h5>{contact.name}</h5>
+                            <h5>{contact.phone}</h5>
+                            <h5>{contact.email}</h5>
+                        </aside>
+                    </main>;
+                })
+            }
+        </div>
     )
 }
 
